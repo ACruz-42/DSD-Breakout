0e041978d47f5de4f36f3965d22724d080ff0941
ldefelice42 <ldefelice42@tntech.edu>
2025-04-13 17:45:25 -0500

    Add files via upload

---
+++ hw_image_generator.vhd
@@ -0,0 +1,486 @@
+--------------------------------------------------------------------------------
+--
+--   FileName:         hw_image_generator.vhd
+--   Dependencies:     none
+--   Design Software:  Quartus II 64-bit Version 12.1 Build 177 SJ Full Version
+--
+--   HDL CODE IS PROVIDED "AS IS."  DIGI-KEY EXPRESSLY DISCLAIMS ANY
+--   WARRANTY OF ANY KIND, WHETHER EXPRESS OR IMPLIED, INCLUDING BUT NOT
+--   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+--   PARTICULAR PURPOSE, OR NON-INFRINGEMENT. IN NO EVENT SHALL DIGI-KEY
+--   BE LIABLE FOR ANY INCIDENTAL, SPECIAL, INDIRECT OR CONSEQUENTIAL
+--   DAMAGES, LOST PROFITS OR LOST DATA, HARM TO YOUR EQUIPMENT, COST OF
+--   PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY OR SERVICES, ANY CLAIMS
+--   BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF),
+--   ANY CLAIMS FOR INDEMNITY OR CONTRIBUTION, OR OTHER SIMILAR COSTS.
+--
+--   Version History
+--   Version 1.0 05/10/2013 Scott Larson
+--     Initial Public Release
+--    
+--------------------------------------------------------------------------------
+--
+-- Altered 10/13/19 - Tyler McCormick 
+-- Test pattern is now 8 equally spaced 
+-- different color vertical bars, from black (left) to white (right)
+
+
+
+-- Altered by Lauren DeFelice to complete pe2
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+ENTITY hw_image_generator IS
+ 
+  PORT(
+	
+	 pixel_clk_m : in std_logic;
+	 led0			: out std_logic;
+	 led1			: out std_logic;
+	 led2			: out std_logic;
+	 channel_a : in std_logic;
+	 channel_b : in std_logic;
+	 fib_index : inout integer;
+	 sw 		  : in std_logic_vector(2 downto 0);
+	 max10_clk:  in std_logic; 
+    disp_ena :  IN   STD_LOGIC;  --display enable ('1' = display time, '0' = blanking time)
+    row      :  IN   INTEGER;    --row pixel coordinate
+    column   :  IN   INTEGER;    --column pixel coordinate
+    red      :  OUT  STD_LOGIC_VECTOR(3 DOWNTO 0) := (OTHERS => '0');  --red magnitude output to DAC
+    green    :  OUT  STD_LOGIC_VECTOR(3 DOWNTO 0) := (OTHERS => '0');  --green magnitude output to DAC
+    blue     :  OUT  STD_LOGIC_VECTOR(3 DOWNTO 0) := (OTHERS => '0')); --blue magnitude output to DAC
+	 
+END hw_image_generator;
+
+ARCHITECTURE behavior OF hw_image_generator IS
+
+signal paddle_x 			: integer := 280;
+signal channel_a_async 	: std_logic;
+signal channel_a_sync 	: std_logic;
+signal channel_b_async 	: std_logic;
+signal channel_b_sync	: std_logic;
+CONSTANT PADDLE_WIDTH 	: INTEGER := 70;
+CONSTANT PADDLE_HEIGHT 	: INTEGER := 10;
+CONSTANT PADDLE_Y 		: INTEGER := 380;
+
+
+------------------------------------------------------------------------------------------------
+  SIGNAL A_sync_0, A_sync_1, A_prev : STD_LOGIC := '0';
+   SIGNAL B_sync_0, B_sync_1 : STD_LOGIC := '0';
+   
+   -- Position tracking
+   SIGNAL current_pos : INTEGER RANGE 20 TO 610 := 280;  -- Initial centered position CHANGE THIS
+   
+   -- The clock was too fast as is, so we slow it down
+   CONSTANT CLK_DIV : INTEGER := 10000;
+   SIGNAL clk_count : INTEGER RANGE 0 TO CLK_DIV-1 := 0;
+   SIGNAL slow_clk_enable : STD_LOGIC := '0';
+   
+   
+   -- Encoder state tracking
+   SIGNAL prev_AB : STD_LOGIC_VECTOR(1 DOWNTO 0) := "00";
+-----------------------------------------------------------------------------------------------
+
+BEGIN
+
+
+	
+	
+		
+	process(disp_ena, row, column, fib_index)
+	 BEGIN
+
+IF(disp_ena = '1' ) THEN 
+ 
+      IF(column < 20) THEN -- left edge
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+     ELSIF(row < 20) THEN -- top edge
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue	<= (OTHERS => '1');
+		  
+		  ----------------------- BEGIN BLOCKS ------------------------------------------------------
+		  
+	  ELSIF(column < 85 AND column > 45 AND row < 80 AND row > 70 AND fib_index /= 0) THEN --1 ON
+		
+        red   <= (OTHERS => '1');		-- WHITE
+        green <= (OTHERS => '1');
+        blue  <= (OTHERS => '1');
+		  
+		  ELSIF(column < 85 AND column > 45 AND row < 80 AND row > 70 AND fib_index = 0) THEN --1 OFF
+		
+        red   <= (OTHERS => '0');		-- BLACK
+        green <= (OTHERS => '0');
+        blue  <= (OTHERS => '0');
+    
+			
+		  
+		ELSIF(column < 85 AND column > 45 AND row < 100 AND row > 90 AND fib_index /= 1) THEN --1 ON
+		  
+        red   <= (OTHERS => '1');
+        green <= (OTHERS => '1');
+        blue  <= (OTHERS => '1');
+		  
+		  ELSIF(column < 85 AND column > 45 AND row < 100 AND row > 90 AND fib_index = 1) THEN --1 OFF
+		  
+        red   <= (OTHERS => '0');
+        green <= (OTHERS => '0');
+        blue  <= (OTHERS => '0');
+  
+ 
+ 
+	 ELSIF(column < 135 AND column > 95 AND row < 80 AND row > 70 AND fib_index /= 2) THEN --2 ON
+
+        red   <= (OTHERS => '1');
+        green <= (OTHERS => '1');
+        blue  <= (OTHERS => '1');
+		  
+		  ELSIF(column < 135 AND column > 95 AND row < 80 AND row > 70 AND fib_index = 2) THEN --2 OFF
+		  
+        red   <= (OTHERS => '0');
+        green <= (OTHERS => '0');
+        blue  <= (OTHERS => '0');
+    
+	 
+	 
+		ELSIF(column < 135 AND column > 95 AND row < 100 AND row > 90 AND fib_index /= 3) THEN --2 ON 
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 135 AND column > 95 AND row < 100 AND row > 90 AND fib_index = 3) THEN --2 OFF 
+		  red   <= (OTHERS => '0');
+        green <= (OTHERS => '0');
+        blue  <= (OTHERS => '0');
+	 
+	 
+	ELSIF(column < 185 AND column > 145 AND row < 80 AND row > 70 AND fib_index /= 4) THEN --3 ON
+       red <= (OTHERS => '1');
+       green  <= (OTHERS => '1');
+		 blue <= (OTHERS => '1');
+		 
+		 ELSIF(column < 185 AND column > 145 AND row < 80 AND row > 70 AND fib_index = 4) THEN --3 OFF
+       red 		<= (OTHERS => '0');
+       green  	<= (OTHERS => '0');
+		 blue 	<= (OTHERS => '0');
+		  
+
+	 
+	ELSIF(column < 185 AND column > 145 AND row < 100 AND row > 90 AND fib_index /= 5) THEN --3 ON
+        red		<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 185 AND column > 145 AND row < 100 AND row > 90 AND fib_index = 5) THEN --3 OFF
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+		  
+
+	 
+	 
+	ELSIF(column < 235 AND column > 195 AND row < 80 AND row > 70 AND fib_index /= 6) THEN --4 ON
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 235 AND column > 195 AND row < 80 AND row > 70 AND fib_index = 6) THEN --4 ON
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+
+	 
+	ELSIF(column < 235 AND column > 195 AND row < 100 AND row > 90 AND fib_index /= 7) THEN --4
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 235 AND column > 195 AND row < 100 AND row > 90 AND fib_index = 7) THEN --4
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+		  
+
+	 
+	ELSIF(column < 285 AND column > 245 AND row < 80 AND row > 70 AND fib_index /= 8) THEN --5
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 285 AND column > 245 AND row < 80 AND row > 70 AND fib_index = 8) THEN --5
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+		  
+
+	 
+	ELSIF(column < 285 AND column > 245 AND row < 100 AND row > 90 AND fib_index /= 9) THEN --5
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 285 AND column > 245 AND row < 100 AND row > 90 AND fib_index = 9) THEN --5
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+		  
+		  
+		  
+	ELSIF(column < 335 AND column > 295 AND row < 80 AND row > 70 AND fib_index /= 10) THEN --6*
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 335 AND column > 295 AND row < 80 AND row > 70 AND fib_index = 10) THEN --6*
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+		  
+		  
+
+	 
+	ELSIF(column < 335 AND column > 295 AND row < 100 AND row > 90 AND fib_index /= 11) THEN --6*
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 335 AND column > 295 AND row < 100 AND row > 90 AND fib_index = 11) THEN --6*
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+		  
+
+	 
+	ELSIF(column < 385 AND column > 345 AND row < 80 AND row > 70 AND fib_index /= 12) THEN --7
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 385 AND column > 345 AND row < 80 AND row > 70 AND fib_index = 12) THEN --7
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+		  
+
+	 
+	ELSIF(column < 385 AND column > 345 AND row < 100 AND row > 90 AND fib_index /= 13) THEN --7
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 385 AND column > 345 AND row < 100 AND row > 90 AND fib_index = 13) THEN --7
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+		  
+
+	 
+	ELSIF(column < 435 AND column > 395 AND row < 80 AND row > 70 AND fib_index /= 14) THEN --8
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 435 AND column > 395 AND row < 80 AND row > 70 AND fib_index = 14) THEN --8
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+		  
+
+	 
+	ELSIF(column < 435 AND column > 395 AND row < 100 AND row > 90 AND fib_index /= 15) THEN --8
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 435 AND column > 395 AND row < 100 AND row > 90 AND fib_index = 15) THEN --8
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+		  
+
+	 
+	ELSIF(column < 485 AND column > 445 AND row < 80 AND row > 70 AND fib_index /= 16) THEN --9
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 485 AND column > 445 AND row < 80 AND row > 70 AND fib_index = 16) THEN --9
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+		  
+
+	 
+  ELSIF(column < 485 AND column > 445 AND row < 100 AND row > 90 AND fib_index /= 17) THEN --9
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 485 AND column > 445 AND row < 100 AND row > 90 AND fib_index = 17) THEN --9
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+		  
+
+	 
+	 
+	ELSIF(column < 535 AND column > 495 AND row < 80 AND row > 70 AND fib_index /= 18) THEN --10
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 535 AND column > 495 AND row < 80 AND row > 70 AND fib_index = 18) THEN --10
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+
+	 
+	 
+	ELSIF(column < 535 AND column > 495 AND row < 100 AND row > 90 AND fib_index /= 19) THEN --10
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 535 AND column > 495 AND row < 100 AND row > 90 AND fib_index = 19) THEN --10
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+		  
+
+	 
+	ELSIF(column < 585 AND column > 545 AND row < 80 AND row > 70 AND fib_index /= 20) THEN --11
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  ELSIF(column < 585 AND column > 545 AND row < 80 AND row > 70 AND fib_index = 20) THEN --11
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+		  
+
+	 
+	 
+	ELSIF(column < 585 AND column > 545 AND row < 100 AND row > 90 AND fib_index /= 21) THEN --11
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+		  
+		  ELSIF(column < 585 AND column > 545 AND row < 100 AND row > 90 AND fib_index = 21) THEN --11
+        red 	<= (OTHERS => '0');
+        green  <= (OTHERS => '0');
+        blue 	<= (OTHERS => '0');
+		  
+
+	 
+	 -----------------------------------------------------------------------------------------------
+		  ELSIF(column < 320 AND column > 310 AND row < 200 AND row > 190) THEN --Ball
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+		  
+			
+			
+		
+--ELSIF(column < 350 AND column > 280 AND row < 390 AND row > 380) THEN  -- Paddle (before) 
+
+					
+	ELSIF(column > paddle_x AND column < paddle_x + paddle_width AND row >= paddle_y AND 
+row < paddle_y + paddle_height) THEN --Paddle
+
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+
+	  ELSIF(column < 640 AND column > 610 ) THEN -- Right edge
+        red 	<= (OTHERS => '1');
+        green  <= (OTHERS => '1');
+        blue 	<= (OTHERS => '1');
+	  ELSE 
+		red 	<= (OTHERS => '0');
+		green <= (OTHERS => '0');
+		blue 	<= (OTHERS => '0');
+	
+      END IF;
+    ELSE                           --blanking time
+      red 	<= (OTHERS => '0');
+      green <= (OTHERS => '0');
+      blue 	<= (OTHERS => '0');
+
+		
+    END IF;
+	end process; 
+	
+	
+	
+  main_process: PROCESS(pixel_clk_m)
+     VARIABLE hundreds, tens, ones : INTEGER;
+     VARIABLE current_AB : STD_LOGIC_VECTOR(1 DOWNTO 0);
+   BEGIN
+     IF rising_edge(pixel_clk_m) THEN
+       -- Synchronize encoder
+       A_sync_0 <= channel_a;
+       A_sync_1 <= A_sync_0;
+       B_sync_0 <= channel_b;
+       B_sync_1 <= B_sync_0;
+       
+       -- Clock divider for debouncing. I'm not sure if it works, but I saw
+ 		-- A bunch of similar project onlines used debouncers, so I made one myself
+       IF clk_count = CLK_DIV-1 THEN
+         clk_count <= 0;
+         slow_clk_enable <= '1';
+       ELSE
+         clk_count <= clk_count + 1;
+         slow_clk_enable <= '0';
+       END IF;
+       
+       -- Only process encoder on the slow clock to debounce
+       IF slow_clk_enable = '1' THEN
+         -- Make encoder state
+         current_AB := A_sync_1 & B_sync_1;
+         
+         -- We detect rotation based on mini-state machine
+         CASE prev_AB & current_AB IS
+           -- These are clockwise
+           WHEN "0001" | "0111" | "1110" | "1000" => 
+             IF current_pos > 20 THEN
+               current_pos <= current_pos - 10;  --  move left
+             END IF;
+             
+           -- These are counter-clockwise
+           WHEN "0010" | "0100" | "1101" | "1011" => 
+             IF current_pos < 610 - paddle_width THEN
+               current_pos <= current_pos + 10;  -- move right
+             END IF;
+             
+           -- No change
+           WHEN OTHERS =>
+             current_pos <= current_pos;
+ 				
+         END CASE;
+         
+         -- Update previous state for next iteration
+         prev_AB <= current_AB;
+       END IF;
+       
+       -- Display logic (always runs)
+       hundreds := current_pos / 100;
+       tens := (current_pos / 10) MOD 10;
+       ones := current_pos MOD 10;
+       
+       
+       -- Set paddle position output
+       paddle_x <= current_pos;
+		 
+     END IF;
+   END PROCESS;
+ 
+
+ 
+end behavior;
---
+++ vga_top_level.vhd
@@ -0,0 +1,133 @@
+-- ECE 4110 VGA example
+--
+-- This code is the top level structural file for code that can
+-- generate an image on a VGA display. The default mode is 640x480 at 60 Hz
+--
+-- Note: This file is not where the pattern/image is produced
+--
+-- Tyler McCormick 
+-- 10/13/2019
+
+
+library   ieee;
+use       ieee.std_logic_1164.all;
+
+entity vga_top_level is
+	
+	port(
+	
+		-- Inputs for image generation
+			 led0			: out std_logic;
+	 led1			: out std_logic;
+	 led2			: out std_logic;
+	 channel_a : in std_logic;
+	 channel_b : in std_logic;
+			fib_index : inout integer;
+				  reset			  : in std_logic;
+     --   key0				: in std_logic;                 
+        hex0     : out std_logic_vector(6 downto 0);  -- 7-segment display output
+		  hex1     : out std_logic_vector(6 downto 0) := "1111111";
+		  hex2     : out std_logic_vector(6 downto 0) := "1111111";
+		  hex3     : out std_logic_vector(6 downto 0) := "1111111";
+		  hex4     : out std_logic_vector(6 downto 0) := "1111111";
+		  hex5     : out std_logic_vector(6 downto 0) := "1111111";
+		  max10_clk	: in std_logic	;
+		sw : in std_logic_vector(2 downto 0);
+		pixel_clk_m		:	IN	STD_LOGIC;     -- pixel clock for VGA mode being used 
+		reset_n_m		:	IN	STD_LOGIC; --active low asycnchronous reset
+		
+		-- Outputs for image generation 
+		
+		h_sync_m		:	OUT	STD_LOGIC;	--horiztonal sync pulse
+		v_sync_m		:	OUT	STD_LOGIC;	--vertical sync pulse 
+		
+		red_m      :  OUT  STD_LOGIC_VECTOR(3 DOWNTO 0) := (OTHERS => '0');  --red magnitude output to DAC
+		green_m    :  OUT  STD_LOGIC_VECTOR(3 DOWNTO 0) := (OTHERS => '0');  --green magnitude output to DAC
+		blue_m     :  OUT  STD_LOGIC_VECTOR(3 DOWNTO 0) := (OTHERS => '0') --blue magnitude output to DAC
+	
+	);
+	
+end vga_top_level;
+
+architecture vga_structural of vga_top_level is
+
+	component vga_pll_25_175 is 
+	
+		port(
+		
+			inclk0		:	IN  STD_LOGIC := '0';  -- Input clock that gets divided (50 MHz for max10)
+			c0			:	OUT STD_LOGIC          -- Output clock for vga timing (25.175 MHz)
+		
+		);
+		
+	end component;
+	
+	component vga_controller is 
+	
+		port(
+		
+			pixel_clk	:	IN	STD_LOGIC;	--pixel clock at frequency of VGA mode being used
+			reset_n		:	IN	STD_LOGIC;	--active low asycnchronous reset
+			h_sync		:	OUT	STD_LOGIC;	--horiztonal sync pulse
+			v_sync		:	OUT	STD_LOGIC;	--vertical sync pulse
+			disp_ena	:	OUT	STD_LOGIC;	--display enable ('1' = display time, '0' = blanking time)
+			column		:	OUT	INTEGER;	--horizontal pixel coordinate
+			row			:	OUT	INTEGER;	--vertical pixel coordinate
+			n_blank		:	OUT	STD_LOGIC;	--direct blacking output to DAC
+			n_sync		:	OUT	STD_LOGIC   --sync-on-green output to DAC
+		
+		);
+		
+	end component;
+	
+	component hw_image_generator is
+	
+		port(
+		pixel_clk_m : in std_logic;
+				 led0			: out std_logic;
+	 led1			: out std_logic;
+	 led2			: out std_logic;
+	 channel_a : in std_logic;
+	 channel_b : in std_logic;
+			fib_index : inout integer;
+			sw : in std_logic_vector(2 downto 0);
+			disp_ena :  IN  STD_LOGIC;  --display enable ('1' = display time, '0' = blanking time)
+			row      :  IN  INTEGER;    --row pixel coordinate
+			column   :  IN  INTEGER;    --column pixel coordinate
+			red      :  OUT STD_LOGIC_VECTOR(3 DOWNTO 0) := (OTHERS => '0');  --red magnitude output to DAC
+			green    :  OUT STD_LOGIC_VECTOR(3 DOWNTO 0) := (OTHERS => '0');  --green magnitude output to DAC
+			blue     :  OUT STD_LOGIC_VECTOR(3 DOWNTO 0) := (OTHERS => '0')   --blue magnitude output to DAC
+		
+		);
+		
+	end component;
+	
+	component pe2_DeFelice is
+    port (      
+			fib_index : inout integer;
+		  reset			  : in std_logic;
+    --    key0				: in std_logic;                 
+        hex0     : out std_logic_vector(6 downto 0);  -- 7-segment display output
+		  hex1     : out std_logic_vector(6 downto 0) := "1111111";
+		  hex2     : out std_logic_vector(6 downto 0) := "1111111";
+		  hex3     : out std_logic_vector(6 downto 0) := "1111111";
+		  hex4     : out std_logic_vector(6 downto 0) := "1111111";
+		  hex5     : out std_logic_vector(6 downto 0) := "1111111";
+		  max10_clk	: in std_logic		  	
+		  
+    );
+end component;
+	
+	signal pll_OUT_to_vga_controller_IN, dispEn : STD_LOGIC;
+	signal rowSignal, colSignal : INTEGER;
+	
+	
+begin
+
+-- Just need 3 components for VGA system 
+	U1	:	vga_pll_25_175 port map(pixel_clk_m, pll_OUT_to_vga_controller_IN);
+	U2	:	vga_controller port map(pll_OUT_to_vga_controller_IN, reset_n_m, h_sync_m, v_sync_m, dispEn, colSignal, rowSignal, open, open);
+	U3	:	hw_image_generator port map(pixel_clk_m, led0, led1, led2, channel_a, channel_b, fib_index, sw, dispEn, rowSignal, colSignal, red_m, green_m, blue_m);
+	U4 :  pe2_DeFelice port map(fib_index, reset, hex0, hex1, hex2, hex3, hex4, hex5, max10_clk);
+
+end vga_structural;
